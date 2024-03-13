21cc4c82f56f3dd39f89f3b2a31208ecab7223cf
Valentin Popov <v.popov.dev@gmail.com>
2024-03-13 11:20:43 +0200

    Fixing ReadInput to return array

--- Airlines.Console/Program.cs
+++ Airlines.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Airlines
 {
@@ -10,17 +12,29 @@ namespace Airlines
             var airlines = Array.Empty<string>();
             var flights = Array.Empty<string>();
 
-            ReadInput("Airport", ref airports);
-            ReadInput("Airline", ref airlines);
-            ReadInput("Flight", ref flights);
+            airports = ReadInput("Airport", airports);
+            airlines = ReadInput("Airline", airlines);
+            flights = ReadInput("Flight", flights);
 
             PrintData("Airport", airports);
             PrintData("Airlines", airlines);
             PrintData("Flights", flights);
+
+            AirportsSorting(airports);
+            AirlinesSorting(airlines);
+            FlightSorting(flights);
+
+            PrintData("Airport", airports);
+            PrintData("Airlines", airlines);
+            PrintData("Flights", flights);
+
+            Search(airports, airlines, flights);
         }
 
-        static void ReadInput(string type, ref string[] data)
+        static string[] ReadInput(string type, string[] currentData)
         {
+            var newData = currentData;
+
             Console.WriteLine($"Enter {type} name, or type 'done' to finish:\n");
 
             while (true)
@@ -35,30 +49,32 @@ namespace Airlines
 
                 if (type == "Airport")
                 {
-                    if (ValidateAirport(input))
+                    if (ValidateAirport(input, newData))
                     {
-                        data = AddData(input, data);
+                        newData = AddData(input, newData);
                     }
                 }
                 else if (type == "Airline")
                 {
-                    if (ValidateAirline(input))
+                    if (ValidateAirline(input, newData))
                     {
-                        data = AddData(input, data);
+                        newData = AddData(input, newData);
                     }
                 }
                 else if (type == "Flight")
                 {
 
-                    if (ValidateFlight(input))
+                    if (ValidateFlight(input, newData))
                     {
-                        data = AddData(input, data);
+                        newData = AddData(input, newData);
                     }
                 }
             }
+
+            return newData;
         }
 
-        static bool ValidateAirport(string airport)
+        static bool ValidateAirport(string airport, string[] airports)
         {
             if (string.IsNullOrEmpty(airport))
             {
@@ -78,10 +94,16 @@ namespace Airlines
                 return false;
             }
 
+            if (airports.Contains(airport))
+            {
+                Console.WriteLine($" Error: Airport name '{airport}' already exist!");
+                return false;
+            }
+
             return true;
         }
 
-        static bool ValidateAirline(string airline)
+        static bool ValidateAirline(string airline, string[] airlines)
         {
             if (string.IsNullOrEmpty(airline))
             {
@@ -95,10 +117,16 @@ namespace Airlines
                 return false;
             }
 
+            if (airlines.Contains(airline))
+            {
+                Console.WriteLine($" Error: Airline name '{airline}' already exist!");
+                return false;
+            }
+
             return true;
         }
 
-        static bool ValidateFlight(string flight)
+        static bool ValidateFlight(string flight, string[] flights)
         {
             if (string.IsNullOrEmpty(flight))
             {
@@ -109,6 +137,12 @@ namespace Airlines
             if (!flight.All(char.IsLetterOrDigit))
             {
                 Console.WriteLine($" Error: Flight code '{flight}' must contain only alphabetic or numeric characters!");
+                return false;
+            }
+
+            if (flights.Contains(flight))
+            {
+                Console.WriteLine($" Error: Flight name '{flight}' already exist!");
                 return false;
             }
 
@@ -140,6 +174,132 @@ namespace Airlines
             else
             {
                 Console.WriteLine(string.Join(", ", data));
+            }
+        }
+
+        static string[] AirportsSorting(string[] airports)
+        {
+            var n = airports.Length;
+            string temp;
+
+            for (int j = 0; j < n - 1; j++)
+            {
+                for (int i = j + 1; i < n; i++)
+                {
+                    if (airports[j].CompareTo(airports[i]) > 0)
+                    {
+                        temp = airports[j];
+                        airports[j] = airports[i];
+                        airports[i] = temp;
+                    }
+                }
+            }
+
+            return airports;
+        }
+
+        static string[] AirlinesSorting(string[] airlines)
+        {
+            int n = airlines.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (string.Compare(airlines[j], airlines[minIndex]) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    string temp = airlines[i];
+                    airlines[i] = airlines[minIndex];
+                    airlines[minIndex] = temp;
+                }
+            }
+
+            return airlines;
+        }
+
+        static string[] FlightSorting(string[] flights)
+        {
+            int n = flights.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (string.Compare(flights[j], flights[minIndex]) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    string temp = flights[i];
+                    flights[i] = flights[minIndex];
+                    flights[minIndex] = temp;
+                }
+            }
+
+            return flights;
+        }
+
+        static void Search(string[] airports, string[] airlines, string[] flights)
+        {
+            AirportsSorting(airports);
+            AirlinesSorting(airlines);
+            AirlinesSorting(flights);
+
+            Console.WriteLine($"\nEnter search term or type 'done' to finish:\n");
+
+            while (true)
+            {
+                string searchTerm = Console.ReadLine();
+
+                bool termFound = false;
+
+                if (string.IsNullOrEmpty(searchTerm))
+                {
+                    Console.WriteLine(" Error: search term cannot be null or empty!");
+                    continue;
+                }
+
+                if (searchTerm.ToLower() == "done")
+                {
+                    Console.WriteLine();
+                    break;
+                }
+
+                if (Array.BinarySearch(airports, searchTerm) >= 0)
+                {
+                    termFound = true;
+                    Console.WriteLine($" {searchTerm} is Airport name.");
+                }
+
+                if (Array.BinarySearch(airlines, searchTerm) >= 0)
+                {
+                    termFound = true;
+                    Console.WriteLine($" {searchTerm} is Airline name.");
+                }
+
+                if (Array.BinarySearch(flights, searchTerm) >= 0)
+                {
+                    termFound = true;
+                    Console.WriteLine($" {searchTerm} is Flight name.");
+                }
+
+                if (!termFound)
+                {
+                    Console.WriteLine($" {searchTerm} was not found.");
+                }
             }
         }
     }
