29aaaa9d19bb3a31357d35446762a982763b6254
Valentin Popov <v.popov.dev@gmail.com>
2024-03-13 14:29:53 +0200

    Adding sorting unit tests

---
+++ Airlines.UnitTest/SortingTests.cs
@@ -0,0 +1,41 @@
+﻿using static Airlines.Program;
+
+namespace Airlines.UnitTest
+{
+    public class SortingTests
+    {
+        [Fact]
+        public void AirportBoubleSortShouldWork()
+        {
+            string[] array = {"aaa", "ccc", "bbb" };
+            string[] result = { "aaa", "bbb", "ccc" };
+
+            string[] sorted = AirportsSorting(array);
+
+            Assert.Equal(sorted, result);
+        }
+
+        [Fact]
+        public void AirlineSelectionSortShouldWork() 
+        {
+            string[] array = { "aaa", "ccc", "bbb" };
+            string[] result = { "aaa", "bbb", "ccc" };
+
+            string[] sorted = AirlinesSorting(array);
+
+            Assert.Equal(sorted, result);
+        }
+
+        [Fact]
+        public void FlightSelectionSortShouldWork()
+        {
+            string[] array = { "aaa", "ccc", "bbb" };
+            string[] result = { "aaa", "bbb", "ccc" };
+
+            string[] sorted = FlightSorting(array);
+
+            Assert.Equal(sorted, result);
+        }
+
+    }
+}
