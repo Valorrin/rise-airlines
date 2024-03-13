ad7dd7eabd044ae6ad88967613ce69e9bc40e755
Valentin Popov <v.popov.dev@gmail.com>
2024-03-13 15:20:19 +0200

    Adding additional validation tests

--- Airlines.UnitTest/ValidationTests.cs
+++ Airlines.UnitTest/ValidationTests.cs
@@ -14,52 +14,67 @@ namespace Airlines.UnitTest
             Assert.True(result);
         }
 
-        [Fact]
-        public void ValidateAirportShouldReturnFalse()
+        [Theory]
+        [InlineData("123")]
+        [InlineData("1234")]
+        [InlineData("abcd")]
+        [InlineData("")]
+        public void ValidateAirportShouldReturnFalse(string airport)
         {
             string[] airports = { "abc", "ccc" };
 
-            bool result = ValidateAirport("ndew", airports);
+            bool result = ValidateAirport(airport, airports);
 
             Assert.False(result);
         }
 
-        [Fact]
-        public void ValidateAirlineShoudReturnTrue()
+        [Theory]
+        [InlineData("12345")]
+        [InlineData("abcde")]
+        [InlineData("ab12")]
+        public void ValidateAirlineShoudReturnTrue(string airline)
         {
             string[] airlines = { "air1", "air2" };
 
-            bool result = ValidateAirline("newnw", airlines);
+            bool result = ValidateAirline(airline, airlines);
 
             Assert.True(result);
         }
 
-        [Fact]
-        public void ValidateAirlineShoudReturnFalse()
+        [Theory]
+        [InlineData("123456")]
+        [InlineData("abcdefg")]
+        [InlineData("")]
+        public void ValidateAirlineShoudReturnFalse(string airline)
         {
             string[] airlines = { "air1", "air2" };
 
-            bool result = ValidateAirline("newnewnew", airlines);
+            bool result = ValidateAirline(airline, airlines);
 
             Assert.False(result);
         }
 
-        [Fact]
-        public void ValidateFlightShoudReturnTrue()
+        [Theory]
+        [InlineData("newflight")]
+        [InlineData("123456")]
+        public void ValidateFlightShoudReturnTrue(string flight)
         {
             string[] flights = { "flight1", "flight2" };
 
-            bool result = ValidateFlight("newflight", flights);
+            bool result = ValidateFlight(flight, flights);
 
             Assert.True(result);
         }
 
-        [Fact]
-        public void ValidateFlightShoudReturnFalse()
+        [Theory]
+        [InlineData("???")]
+        [InlineData("?a1")]
+        [InlineData("")]
+        public void ValidateFlightShoudReturnFalse(string flight)
         {
             string[] flights = { "flight1", "flight2" };
 
-            bool result = ValidateFlight("???", flights);
+            bool result = ValidateFlight(flight, flights);
 
             Assert.False(result);
         }
