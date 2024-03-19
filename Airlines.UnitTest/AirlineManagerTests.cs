167a648ade0ea38130044c2b5a89060796adc682
Valentin Popov <v.popov.dev@gmail.com>
2024-03-19 13:11:34 +0200

    Add unit tests for the managers

---
+++ Airlines.UnitTest/AirlineManagerTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace Airlines.Console.Tests;
+
+public class AirlineManagerTests
+{
+    [Fact]
+    public void Add_Airline_Successfully()
+    {
+        var airlineManager = new AirlineManager();
+        var airlineName = "Test";
+
+        airlineManager.Add(airlineName);
+
+        Assert.Contains(airlineName, airlineManager.Airlines);
+    }
+
+    [Fact]
+    public void Add_Airline_With_Long_Name_Fails()
+    {
+        var airlineManager = new AirlineManager();
+        var longAirlineName = "TooLongAirlineName";
+
+        airlineManager.Add(longAirlineName);
+
+        Assert.Empty(airlineManager.Airlines);
+    }
+
+    [Fact]
+    public void Add_Airline_With_Duplicate_Name_Fails()
+    {
+        var airlineManager = new AirlineManager();
+        var airlineName = "Test";
+        airlineManager.Add(airlineName);
+
+        airlineManager.Add(airlineName);
+
+        _ = Assert.Single(airlineManager.Airlines);
+    }
+
+    [Theory]
+    [InlineData("Test", true)]
+    [InlineData("NonexistentAirline", false)]
+    public void Search_Airline(string searchTerm, bool expectedResult)
+    {
+        var airlineManager = new AirlineManager();
+        airlineManager.Add("Test");
+
+        var writer = new StringWriter();
+        System.Console.SetOut(writer);
+
+        airlineManager.Search(searchTerm);
+        var output = writer.ToString().Trim();
+
+        if (expectedResult)
+        {
+            Assert.Contains(searchTerm, output);
+        }
+        else
+        {
+            Assert.DoesNotContain(searchTerm, output);
+        }
+    }
+
+    [Fact]
+    public void Print_Airlines()
+    {
+        var airlineManager = new AirlineManager();
+        airlineManager.Add("AAA");
+        airlineManager.Add("BBB");
+        airlineManager.Add("CCC");
+
+        var writer = new StringWriter();
+        System.Console.SetOut(writer);
+
+        airlineManager.Print();
+        var output = writer.ToString().Trim();
+
+        Assert.Contains("AAA", output);
+        Assert.Contains("BBB", output);
+        Assert.Contains("CCC", output);
+    }
+}
---
+++ Airlines.UnitTest/AirportManagerTests.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Airlines.Console.Tests;
+
+public class AirportManagerTests
+{
+    [Fact]
+    public void Add_Airport_Successfully()
+    {
+        var airportManager = new AirportManager();
+        var airportCode = "ABC";
+
+        airportManager.Add(airportCode);
+
+        Assert.Contains(airportCode, airportManager.Airports);
+    }
+
+    [Fact]
+    public void Add_Airport_With_Long_Code_Fails()
+    {
+        var airportManager = new AirportManager();
+        var longAirportCode = "TooLongAirportCode";
+
+        airportManager.Add(longAirportCode);
+
+        Assert.Empty(airportManager.Airports);
+    }
+
+    [Fact]
+    public void Add_Airport_With_Duplicate_Code_Fails()
+    {
+        var airportManager = new AirportManager();
+        var airportCode = "ABC";
+        airportManager.Add(airportCode);
+
+        airportManager.Add(airportCode);
+
+        _ = Assert.Single(airportManager.Airports);
+    }
+
+    [Theory]
+    [InlineData("ABC", true)]
+    [InlineData("NonexistentAirport", false)]
+    public void Search_Airport(string searchTerm, bool expectedResult)
+    {
+        var airportManager = new AirportManager();
+        airportManager.Add("ABC");
+
+        var writer = new StringWriter();
+        System.Console.SetOut(writer);
+
+        airportManager.Search(searchTerm);
+        var output = writer.ToString().Trim();
+
+        if (expectedResult)
+        {
+            Assert.Contains(searchTerm, output);
+        }
+        else
+        {
+            Assert.DoesNotContain(searchTerm, output);
+        }
+    }
+
+    [Fact]
+    public void Print_Airports()
+    {
+        var airportManager = new AirportManager();
+        airportManager.Add("AAA");
+        airportManager.Add("BBB");
+        airportManager.Add("CCC");
+
+        var writer = new StringWriter();
+        System.Console.SetOut(writer);
+
+        airportManager.Print();
+        var output = writer.ToString().Trim();
+
+        Assert.Contains("AAA", output);
+        Assert.Contains("BBB", output);
+        Assert.Contains("CCC", output);
+    }
+}
