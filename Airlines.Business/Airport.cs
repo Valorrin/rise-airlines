5d768bf345e4c1e949add450c708be665359e4cb
Valentin Popov <v.popov.dev@gmail.com>
2024-03-20 14:42:47 +0200

    Add Airline and Airport classes

---
+++ Airlines.Business/Airport.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace Airlines.Business;
+public class Airport
+{
+    public string Name { get; set; }
+
+    public string City { get; set; }
+
+    public string Country { get; set; }
+}
