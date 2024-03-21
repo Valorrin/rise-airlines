45d9b093943df6f2343a7f5d3d45de8c7bf25585
Valentin Popov <v.popov.dev@gmail.com>
2024-03-21 16:49:34 +0200

    Fix formating 2

--- Airlines.UnitTest/SortExtentionTests.cs
+++ Airlines.UnitTest/SortExtentionTests.cs
@@ -1,40 +1,36 @@
 ﻿using Airlines.Business;
-using Xunit;
-using System;
-using System.Collections.Generic;
 
-namespace Airlines.Console.Tests
+namespace Airlines.Console.Tests;
+
+public class SortExtensionsTests
 {
-    public class SortExtensionsTests
+    [Fact]
+    public void Sort_FlightManager_Sorts_In_Ascending_Order()
     {
-        [Fact]
-        public void Sort_FlightManager_Sorts_In_Ascending_Order()
-        {
-            var manager = new FlightManager();
-            manager.Add("FL123");
-            manager.Add("FL456");
-            manager.Add("FL789");
-            manager.Add("FL101");
-            var expected = new List<string> { "FL101", "FL123", "FL456", "FL789" };
+        var manager = new FlightManager();
+        manager.Add("FL123");
+        manager.Add("FL456");
+        manager.Add("FL789");
+        manager.Add("FL101");
+        var expected = new List<string> { "FL101", "FL123", "FL456", "FL789" };
 
-            manager.Sort();
+        manager.Sort();
 
-            Assert.Equal(expected, manager.Flights);
-        }
+        Assert.Equal(expected, manager.Flights);
+    }
 
-        [Fact]
-        public void SortDesc_FlightManager_Sorts_In_Descending_Order()
-        {
-            var manager = new FlightManager();
-            manager.Add("FL123");
-            manager.Add("FL456");
-            manager.Add("FL789");
-            manager.Add("FL101");
-            var expected = new List<string> { "FL789", "FL456", "FL123", "FL101" };
+    [Fact]
+    public void SortDesc_FlightManager_Sorts_In_Descending_Order()
+    {
+        var manager = new FlightManager();
+        manager.Add("FL123");
+        manager.Add("FL456");
+        manager.Add("FL789");
+        manager.Add("FL101");
+        var expected = new List<string> { "FL789", "FL456", "FL123", "FL101" };
 
-            manager.SortDesc();
+        manager.SortDesc();
 
-            Assert.Equal(expected, manager.Flights);
-        }
+        Assert.Equal(expected, manager.Flights);
     }
 }
