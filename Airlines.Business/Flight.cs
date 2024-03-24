0bd1a2d4edb30657bb0008d7a4ff46f0de25c0dd
Valentin Popov <v.popov.dev@gmail.com>
2024-03-24 10:52:17 +0200

    Create Flight class

---
+++ Airlines.Business/Flight.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Airlines.Business;
+public class Flight
+{
+    public string Id { get; set; }
+
+    public string DepartureAirport { get; set; }
+
+    public string ArrivalAirport { get; set; }
+}
