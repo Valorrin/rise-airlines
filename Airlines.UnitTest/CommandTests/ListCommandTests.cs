a95ab9f8f6bc336ae5e748e55670b8c847c9b710
Valentin Popov <v.popov.dev@gmail.com>
2024-03-29 12:44:35 +0200

    Add ReserveCommandsTests

---
+++ Airlines.UnitTest/CommandTests/ListCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using Airlines.Business.Commands.ListingCommands;
+using Airlines.Business.Managers;
+
+
+namespace Airlines.UnitTests.CommandTests;
+public class ListCommandTests
+{
+    [Fact]
+    public void CreateListDataCommand_ReturnsInstanceOfListDataCommand()
+    {
+        var airportManager = new AirportManager();
+        var inputData = "Test Country";
+        var from = "Country";
+
+        var command = ListDataCommand.CreateListDataCommand(airportManager, inputData, from);
+
+        Assert.NotNull(command);
+        _ = Assert.IsType<ListDataCommand>(command);
+    }
+}
