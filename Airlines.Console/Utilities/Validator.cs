b8d5ee34332a047944d46eea2dcea7d18398edd3
Valentin Popov <v.popov.dev@gmail.com>
2024-03-29 19:32:04 +0200

    Add validations for: Airport data, Airline data, Flight Data

--- Airlines.Console/Utilities/Validator.cs
+++ Airlines.Console/Utilities/Validator.cs
@@ -1,6 +1,7 @@
 ﻿
 using Airlines.Business.Managers;
 using Airlines.Business.Models;
+using System.Xml.Linq;
 
 namespace Airlines.Console.Utilities;
 public class Validator
@@ -25,6 +26,18 @@ public class Validator
         var country = dataParts[3];
 
         if (string.IsNullOrEmpty(id))
+        {
+            return false;
+        }
+        if (string.IsNullOrEmpty(name))
+        {
+            return false;
+        }
+        if (string.IsNullOrEmpty(city))
+        {
+            return false;
+        }
+        if (string.IsNullOrEmpty(country))
         {
             return false;
         }
@@ -73,6 +86,109 @@ public class Validator
         }
 
         return true;
+    }
 
+    public bool ValidateAirlineData(string data)
+    {
+        var dataParts = data.Split(", ").ToArray();
+        var id = dataParts[0];
+        var name = dataParts[1];
+
+        if (string.IsNullOrEmpty(id))
+        {
+            return false;
+        }
+        if (string.IsNullOrEmpty(name))
+        {
+            return false;
+        }
+
+        if (_airlineManager.Airlines.ContainsKey(id))
+        {
+            System.Console.WriteLine(" Error: An airline with the same ID already exists.");
+            return false;
+        }
+
+        if (id.Length is < 2 or > 4)
+        {
+            return false;
+        }
+
+        foreach (var c in id)
+        {
+            if (!char.IsLetterOrDigit(c))
+            {
+                return false;
+            }
+        }
+        foreach (var c in name)
+        {
+            if (!char.IsLetter(c) || c == ' ')
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool ValidateFlightData(string data)
+    {
+        var dataParts = data.Split(", ").ToArray();
+        var id = dataParts[0];
+        var departureAirportId = dataParts[1];
+        var arrivalAirportId = dataParts[2];
+
+        if (string.IsNullOrEmpty(id))
+        {
+            return false;
+        }
+        if (string.IsNullOrEmpty(departureAirportId))
+        {
+            return false;
+        }
+        if (string.IsNullOrEmpty(arrivalAirportId))
+        {
+            return false;
+        }
+
+        if (_flightManager.Flights.Any(x => x.Id == id))
+        {
+            System.Console.WriteLine(" Error: A flight with the same ID already exists.");
+            return false;
+        }
+
+        if (departureAirportId.Length is < 2 or > 4)
+        {
+            return false;
+        }
+        if (arrivalAirportId.Length is < 2 or > 4)
+        {
+            return false;
+        }
+
+        foreach (var c in id)
+        {
+            if (!char.IsLetterOrDigit(c))
+            {
+                return false;
+            }
+        }
+        foreach (var c in departureAirportId)
+        {
+            if (!char.IsLetterOrDigit(c))
+            {
+                return false;
+            }
+        }
+        foreach (var c in arrivalAirportId)
+        {
+            if (!char.IsLetterOrDigit(c))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
