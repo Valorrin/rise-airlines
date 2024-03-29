dca3b492f312157dcaa69f1db61d73d381463f92
Valentin Popov <v.popov.dev@gmail.com>
2024-03-29 19:09:13 +0200

    Create Validator in the Presentation layer

---
+++ Airlines.Console/Utilities/Validator.cs
@@ -0,0 +1,78 @@
+﻿
+using Airlines.Business.Managers;
+using Airlines.Business.Models;
+
+namespace Airlines.Console.Utilities;
+public class Validator
+{
+    private readonly AirportManager _airportManager;
+    private readonly AirlineManager _airlineManager;
+    private readonly FlightManager _flightManager;
+
+    public Validator(AirportManager airportManager, AirlineManager airlineManager, FlightManager flightManager)
+    {
+        _airportManager = airportManager;
+        _airlineManager = airlineManager;
+        _flightManager = flightManager;
+    }
+
+    public bool ValidateAirportData(string data)
+    {
+        var dataParts = data.Split(", ").ToArray();
+        var id = dataParts[0];
+        var name = dataParts[1];
+        var city = dataParts[2];
+        var country = dataParts[3];
+
+        if (string.IsNullOrEmpty(id))
+        {
+            return false;
+        }
+
+        if (_airportManager.Airports.ContainsKey(id))
+        {
+            System.Console.WriteLine(" Error: An airport with the same ID already exists.");
+            return false;
+        }
+
+        if (id.Length is < 2 or > 4)
+        {
+            return false;
+        }
+
+        foreach (var c in id)
+        {
+            if (!char.IsLetterOrDigit(c))
+            {
+                return false;
+            }
+        }
+
+        foreach (var c in name)
+        {
+            if (!char.IsLetter(c) || c == ' ')
+            {
+                return false;
+            }
+        }
+
+        foreach (var c in city)
+        {
+            if (!char.IsLetter(c) || c == ' ')
+            {
+                return false;
+            }
+        }
+
+        foreach (var c in country)
+        {
+            if (!char.IsLetter(c) || c == ' ')
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
