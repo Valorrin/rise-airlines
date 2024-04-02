0cf5ce596f68f4ef6172b83c076a8b09994eb68f
Valentin Popov <v.popov.dev@gmail.com>
2024-04-02 12:38:46 +0300

    Add Route find commands

---
+++ Airlines.Business/Commands/RouteCommands/RouteFindCommand.cs
@@ -0,0 +1,18 @@
+﻿using Airlines.Business.Managers;
+
+namespace Airlines.Business.Commands.RouteCommands;
+public class RouteFindCommand : ICommand
+{
+    private readonly RouteManager _routeManager;
+    private readonly string _startAirportId;
+
+    private RouteFindCommand(RouteManager routeManager, string startAirportId)
+    {
+        _routeManager = routeManager;
+        _startAirportId = startAirportId;
+    }
+
+    public void Execute() => _routeManager.Print(_startAirportId);
+
+    public static RouteFindCommand CreateRouteFindCommand(RouteManager routeManager, string startAirportId) => new(routeManager, startAirportId);
+}
