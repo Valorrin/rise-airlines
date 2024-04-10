a90e6a9b38676b517b2b85f14313fd74dfe73501
Valentin Popov <v.popov.dev@gmail.com>
2024-04-10 19:53:29 +0300

    Task 22 crfix

--- Db/Scripts/Task-22-Airlines-Database-Create-tables.sql
+++ Db/Scripts/Task-22-Airlines-Database-Create-tables.sql
@@ -8,26 +8,27 @@ USE AirlinesDB;
 GO
 
 CREATE TABLE Airports (
-    [Name] nvarchar(255),
-    Country nvarchar(255),
-    City nvarchar(255),
-    Code CHAR(3),
-    Runways int,
-    Founded DATE
+    [Name] NVARCHAR(255),
+    [Country] NVARCHAR(75),
+    [City] NVARCHAR(100),
+    [Code] NVARCHAR(3),
+    [Runways] INT,
+    [Founded] DATE
 );
 
 CREATE TABLE Airlines (
-    [Name] nvarchar(255),
-    FleetSize int,
-    [Description] text
+    [Name] NVARCHAR(6),
+    [Founded] DATE,
+    [FleetSize] INT,
+    [Description] TEXT
 );
 
 CREATE TABLE Flights (
-    Number nvarchar(255),
-    DepartureAirport nvarchar(255),
-    ArrivalAirport nvarchar(255),
-    DepartureDateTime DATETIME,
-    ArrivalDateTime DATETIME
+    [Number] NVARCHAR(255),
+    [DepartureAirport] NVARCHAR(3),
+    [ArrivalAirport] NVARCHAR(3),
+    [DepartureDateTime] NVARCHAR,
+    [ArrivalDateTime] NVARCHAR
 );
 
 INSERT INTO Flights (Number, DepartureAirport, ArrivalAirport, DepartureDateTime, ArrivalDateTime)
@@ -36,8 +37,11 @@ INSERT INTO Flights (Number, DepartureAirport, ArrivalAirport, DepartureDateTime
 INSERT INTO Flights (Number, DepartureAirport, ArrivalAirport, DepartureDateTime, ArrivalDateTime)
     VALUES ('FL456', 'LAX', 'ORD', '2024-04-15 17:20:00', '2024-04-16 10:15:00');
 
-SELECT * FROM Flights WHERE Number = 'FL123';
+SELECT * FROM Flights 
+    WHERE Number = 'FL123';
 
-UPDATE Flights SET ArrivalAirport = 'DFW' WHERE Number = 'FL456';
+UPDATE Flights SET ArrivalAirport = 'DFW' 
+    WHERE Number = 'FL456';
 
-DELETE FROM Flights WHERE Number = 'FL123';
+DELETE FROM Flights 
+    WHERE Number = 'FL123';
