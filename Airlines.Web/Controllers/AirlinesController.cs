bc60fd7de4bb737503b36b24a4e2bbbcb3042422
Valentin Popov <v.popov.dev@gmail.com>
2024-05-06 20:13:23 +0300

    Create controllers and views for airports, airlines, and flights

---
+++ Airlines.Web/Controllers/AirlinesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Airlines.Web.Controllers;
+public class AirlinesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Airlines.Web/Controllers/FlightsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Airlines.Web.Controllers;
+public class FlightsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
