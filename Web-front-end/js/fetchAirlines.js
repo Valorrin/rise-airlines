a163e3e32c3f245a948dde7c66de91e805df4f40
Valentin Popov <v.popov.dev@gmail.com>
2024-05-20 22:10:18 +0300

    Airline fetching

--- Web-front-end/js/fetchAirlines.js
+++ Web-front-end/js/fetchAirlines.js
@@ -1,9 +1,40 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const airlinesData = await getAllAsync("airlines");
-    
-    if (airlinesData) {
-      populateTable(airlinesData, 'airlines-tbody');
-    } else {
+  const type = "airlines";
+  const airlineData = await getAllAsync(type);
+
+  if (airlineData) {
+      console.log("Airline data is available.");
+      const tableBody = document.getElementById('airlines-tbody');
+      tableBody.innerHTML = ''; // Clear any existing rows
+
+      // Append airport data to table
+      airlineData.forEach(airline => {
+          const row = document.createElement('tr');
+          row.setAttribute('data-id', airline.airportId);
+
+          row.innerHTML = `
+              <td>${airline.name}</td>
+              <td>${new Date(airline.founded).toLocaleDateString()}</td>
+              <td>${airline.fleetSize}</td>
+              <td>${airline.description}</td>
+              <td>
+                  <a><button class="edit-btn">Edit</button></a>
+                  <a><button class="delete-btn">Delete</button></a>
+              </td>
+          `;
+
+          row.querySelector('.delete-btn').addEventListener('click', async () => {
+              const success = await deleteAsync(type, airline.airlineId);
+              if (success) {
+                  tableBody.removeChild(row);
+              } else {
+                  console.error("Failed to delete airline data.");
+              }
+          });
+
+          tableBody.appendChild(row);
+      });
+  } else {
       console.error("Failed to fetch airline data.");
-    }
-  });
+  }
+});
